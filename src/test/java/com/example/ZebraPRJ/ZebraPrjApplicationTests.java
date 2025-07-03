a12ffdb09b52de6b5d59b98169485744f620db4a
Alexey Bykov <bykov_a_a@mail.ru>
2025-07-04 03:20:32 +0400

    Add hello service

---
+++ src/test/java/com/example/ZebraPRJ/ZebraPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ZebraPRJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZebraPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
