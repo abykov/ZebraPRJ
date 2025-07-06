6d10dd76d9f3bfa30a9872a56fa9738773949e09
Alexey Bykov <bykov_a_a@mail.ru>
2025-07-07 01:18:27 +0400

    Add Swagger

--- src/main/java/com/example/ZebraPRJ/ZebraController.java
+++ src/main/java/com/example/ZebraPRJ/ZebraController.java
@@ -1,11 +1,21 @@
 package com.example.ZebraPRJ;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "ZebraAPI", description = "API for Zebra application")
 public class ZebraController {
     @GetMapping("/hello")
+    @Operation(summary = "Get Hello message", description = "Return simple hardcoded message 'Hello'")
+    @ApiResponses(value = {
+            @ApiResponse (responseCode = "200", description = "Successfully retrieved greeting"),
+            @ApiResponse (responseCode = "500", description = "=( nternal server error =(")
+    })
     public String sayHello(){
         return "Hello";
     }
