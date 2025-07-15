44f3b3a1538bbecec80d9832be262b196de28ee9
Alexey Bykov <bykov_a_a@mail.ru>
2025-07-16 01:49:47 +0400

    Add DB integration. Local (on host) test

--- src/main/java/com/example/ZebraPRJ/ZebraPrjController.java
+++ src/main/java/com/example/ZebraPRJ/ZebraPrjController.java
@@ -1,20 +1,28 @@
 package com.example.ZebraPRJ;
 
 import com.example.ZebraPRJ.model.User;
+import com.example.ZebraPRJ.repository.UserRepository;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @Tag(name = "ZebraAPI", description = "API for Zebra application")
 public class ZebraPrjController {
+
+    private final UserRepository userRepository;
+
+    public ZebraPrjController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
 
     @GetMapping(value = "/hello", produces = MediaType.TEXT_PLAIN_VALUE)
     @Operation(summary = "Get Hello message", description = "Return simple hardcoded message 'Hello'")
@@ -34,12 +42,6 @@ public class ZebraPrjController {
             @ApiResponse(responseCode = "500", description = "=( Internal server error =(")
     })
     public List<User> getUsers(){
-        return Arrays.asList(
-                new User(1L, "Alice Smith", "alice@example.com", 25),
-                new User(2L, "Bob Johnson", "bob@example.com", 30),
-                new User(3L, "Charlie Brown", "charlie@example.com", 28),
-                new User(4L, "Diana Wilson", "diana@example.com", 35),
-                new User(5L, "Eve Davis", "eve@example.com", 22)
-        );
+        return userRepository.findAll();
     }
 }
