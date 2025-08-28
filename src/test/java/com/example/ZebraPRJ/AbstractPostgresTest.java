400f9302fae794e4456d5d38dd402f49131e94ea
Alexey Bykov <bykov_a_a@mail.ru>
2025-08-28 18:44:06 +0400

    Add AbstractPostgresTest to start Testcontainers in one place

---
+++ src/test/java/com/example/ZebraPRJ/AbstractPostgresTest.java
@@ -0,0 +1,15 @@
+package com.example.ZebraPRJ;
+
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class AbstractPostgresTest {
+
+    @Container
+    @ServiceConnection
+    protected static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:15-alpine");
+}
--- src/test/java/com/example/ZebraPRJ/ZebraPrjControllerTest.java
+++ src/test/java/com/example/ZebraPRJ/ZebraPrjControllerTest.java
@@ -13,15 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testcontainers.DockerClientFactory;
 
 import java.time.LocalDate;
@@ -37,16 +33,9 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 // 1. Tell Spring Boot to run on a random port to enable web testing
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers
 // 2. Override ddl-auto to have Hibernate create the schema for us in the fresh container
 @TestPropertySource(properties = "spring.jpa.hibernate.ddl-auto=create-drop")
-class ZebraPrjControllerTest {
-
-
-
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
+class ZebraPrjControllerTest extends AbstractTestNGSpringContextTests {
 
     // 3. Autowire both the repository and the TestRestTemplate
     @Autowired
